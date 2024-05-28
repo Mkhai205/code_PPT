afcc1807c8b7621d2a06b1ce20bc88947dea56e7
MKhai205 <manhkhai123459876@gmail.com>
2024-05-29 02:11:23 +0700

    chore: Add input and output files for calculations

--- polynomial_interpolation.c
+++ polynomial_interpolation.c
@@ -1,104 +1,160 @@
 #include "stdio.h"
 #include "math.h"
 
-int main(){
-    // Declare the variables
-    int i , j, k, degreeOfPolynomial, numberOfPoints;
-    
-    // Enter the data
+int degreeOfPolynomial, numberOfPoints, numberOfCoefficients;
+double x[100], y[100];
+// Array X to store the values of sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
+// Array Y to store the values of sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
+double X[11], Y[11];
+double polynomialCoefficientsMatrix[11][12], interpolationCoefficients[11];
+
+void input_data(){
     printf("\nEnter the number of data pairs: ");
     scanf("%d", &numberOfPoints);
     
-    
-    double x[numberOfPoints], y[numberOfPoints];
     // Input x-values
     printf("\nEnter the x-axis values:\n"); 
-    for(i=0; i<numberOfPoints; i++){
+    for(int i=0; i<numberOfPoints; i++){
         scanf("%lf", &x[i]);
     }
     
     // Input y-values
     printf("\nEnter the y-axis values:\n"); 
-    for(i=0; i<numberOfPoints; i++){
+    for(int i=0; i<numberOfPoints; i++){
         scanf("%lf", &y[i]);
     }
     
     // Enter the degree of polynomial
     printf("\nWhat degree of Polynomial do you want to use for the fit?\n");
-    scanf("%d", &degreeOfPolynomial); 
+    scanf("%d", &degreeOfPolynomial);
+    numberOfCoefficients = degreeOfPolynomial + 1;
+}
 
-    // Array X to store the values of sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-    // Array Y to store the values of sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
-    double X[2*degreeOfPolynomial+1], Y[degreeOfPolynomial+1];
+int file_data(){
+    FILE *fp;
+    char filename[] = "input.txt";
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return 0;
+    }
+    fscanf(fp, "%d", &numberOfPoints);
+    for(int i=0; i<numberOfPoints; i++){
+        fscanf(fp, "%lf", &x[i]);
+    }
+    for(int i=0; i<numberOfPoints; i++){
+        fscanf(fp, "%lf", &y[i]);
+    }
+    fscanf(fp, "%d", &degreeOfPolynomial);
+    numberOfCoefficients = degreeOfPolynomial + 1;
+    fclose(fp);
+    printf("Read file successfully\n");
+    return 1;
+}
+
+void data_preprocessing(){
     // Consecutive positions of the array will store N,sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-    for(i=0; i<2*degreeOfPolynomial+1; i++){
+    for(int i=0; i<2*degreeOfPolynomial+1; i++){
         X[i] = 0;
-        for(j=0; j<numberOfPoints; j++){
+        for(int j=0; j<numberOfPoints; j++){
             X[i] = X[i] + pow(x[j],i); 
         }
     }
     // Consecutive positions will store sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
-    for(i=0;i<degreeOfPolynomial+1;i++){    
+    for(int i=0;i<degreeOfPolynomial+1;i++){    
         Y[i] = 0;
-        for(j=0;j<numberOfPoints;j++){ 
+        for(int j=0;j<numberOfPoints;j++){ 
             Y[i] = Y[i] + pow(x[j],i)*y[j]; 
         }
     }
-    
-    // B is the Normal matrix(augmented) that will store the equations, 'a' is for value of the final coefficients
-    double B[degreeOfPolynomial+1][degreeOfPolynomial+2], a[degreeOfPolynomial+1];
     // Build the Normal matrix by storing the corresponding coefficients at the right positions except the last column of the matrix
-    for(i=0;i<=degreeOfPolynomial;i++){ 
-        for(j=0; j<=degreeOfPolynomial; j++){
-            B[i][j] = X[i+j];
+    for(int i=0;i<=degreeOfPolynomial;i++){ 
+        for(int j=0; j<=degreeOfPolynomial; j++){
+            polynomialCoefficientsMatrix[i][j] = X[i+j];
         }
     }
-    // Build the values of Y as the last column of B(Normal Matrix but augmented)
-    for(i=0; i<=degreeOfPolynomial; i++){ 
-        B[i][degreeOfPolynomial+1] = Y[i]; 
+    // Build the values of Y as the last column of polynomialCoefficientsMatrix(Normal Matrix but augmented)
+    for(int i=0; i<=degreeOfPolynomial; i++){ 
+        polynomialCoefficientsMatrix[i][degreeOfPolynomial+1] = Y[i]; 
     }
-    
-
-    // Solve the system of equations according to gaussian theory
-    int numberOfCoefficients = degreeOfPolynomial + 1;
+    // The number of coefficients in the augmented matrix
     printf("\nThe Augmented Matrix is as follows:\n");
-    for(i=0; i<numberOfCoefficients; i++){
-        for(j=0; j<numberOfCoefficients; j++){
-            printf("%lf\t", B[i][j]);
+    for(int i=0; i<numberOfCoefficients; i++){
+        for(int j=0; j<numberOfCoefficients; j++){
+            printf("%lf\t", polynomialCoefficientsMatrix[i][j]);
         }
-        printf("|   %lf\n", B[i][numberOfCoefficients]);
+        printf("|   %lf\n", polynomialCoefficientsMatrix[i][numberOfCoefficients]);
     }
+}
+
+void solve_by_gaussian(){
+    // Solve the system of equations according to gaussian theory
     // Loop to perform the gauss elimination
-    for(i=0; i<numberOfCoefficients-1; i++){ 
-        for(k=i+1; k<numberOfCoefficients; k++){
-            double tmp = B[k][i]/B[i][i];
-            for(j=0; j<=numberOfCoefficients; j++){
-                B[k][j] = B[k][j] - tmp*B[i][j];
+    for(int i=0; i<numberOfCoefficients-1; i++){ 
+        for(int k=i+1; k<numberOfCoefficients; k++){
+            double tmp = polynomialCoefficientsMatrix[k][i]/polynomialCoefficientsMatrix[i][i];
+            for(int j=0; j<=numberOfCoefficients; j++){
+                polynomialCoefficientsMatrix[k][j] = polynomialCoefficientsMatrix[k][j] - tmp*polynomialCoefficientsMatrix[i][j];
             }
         }
     }
     // Back substitution
-    for(i=numberOfCoefficients-1; i>=0; i--){ 
-        a[i] = B[i][numberOfCoefficients];
-        for(j=numberOfCoefficients-1; j>i; j--){
-            a[i] = a[i] - B[i][j]*a[j];
+    for(int i=numberOfCoefficients-1; i>=0; i--){
+        interpolationCoefficients[i] = polynomialCoefficientsMatrix[i][numberOfCoefficients];
+        for(int j=numberOfCoefficients-1; j>i; j--){
+            interpolationCoefficients[i] = interpolationCoefficients[i] - polynomialCoefficientsMatrix[i][j]*interpolationCoefficients[j];
         }
-        a[i] = a[i]/B[i][i];
+        interpolationCoefficients[i] = interpolationCoefficients[i]/polynomialCoefficientsMatrix[i][i];
     }
+}
 
-
+void output(){
     // Print the results
     printf("\nThe values of the coefficients are as follows:\n");
-    for(i=0; i<numberOfCoefficients; i++){
-        printf("%c = %lf\n", 'a'+i, a[i]);
+    for(int i=0; i<numberOfCoefficients; i++){
+        printf("%c = %lf\n", 'a'+i, interpolationCoefficients[i]);
     }
 
     printf("\nThe fitted Polynomial is given by:\ny = ");
-    for(i=0; i<numberOfCoefficients; i++){
-        printf("%lf", a[i]);
+    for(int i=0; i<numberOfCoefficients; i++){
+        printf("%lf", interpolationCoefficients[i]);
         if (i != 0)  printf("x^%d", i);
         if (i != numberOfCoefficients-1)  printf(" + ");
     }
-    printf("\n\n-------------------------------------------------------------");
+    printf("\n\n-------------------------------------------------------------\n");
+}
+
+void file_output(){
+    FILE *fp;
+    char filename[] = "output.txt";
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    for(int i=0; i<numberOfCoefficients; i++){
+        fprintf(fp, "%lf\n", interpolationCoefficients[i]);
+    }
+    for(int i=0; i<numberOfPoints; i++){
+        fprintf(fp, "%lf ", x[i]);
+    }
+    fprintf(fp, "\n");
+    for(int i=0; i<numberOfPoints; i++){
+        fprintf(fp, "%lf ", y[i]);
+    }
+    fclose(fp);
+    printf("Write file successfully\n");
+}
+
+int main(){
+    if(file_data()){
+        data_preprocessing();
+        solve_by_gaussian();
+        output();
+        file_output();
+    }
+    else{
+        printf("Cannot read file");
+    }
     return 0;
 }
